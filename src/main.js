5407119c9666a1658177b6476a413b3376f2b351
ohmyray <ohmyray@163.com>
2020-03-10 01:22:20 +0800

    chore(main.js): 配置全局样式及Vant

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 全局样式表
+import '@/assets/css/rest.css'
+// Vant-ui引入
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
