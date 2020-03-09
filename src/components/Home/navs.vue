fad47b0ebae7306dfd3e5ae4c690889beffad07e
ohmyray <ohmyray@163.com>
2020-03-10 02:59:09 +0800

    feat(navs.vue)：完成首页功能页面

---
+++ src/components/Home/navs.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="navs">
+    <div :key="index"
+         v-for="(item, index) of icon"
+         class="item">
+      <div class="circle"
+           :style="{backgroundColor:item.bg}">
+        <van-icon class="iconfont"
+                  :class="item.icon" />
+      </div>
+      <span>{{item.text}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: Array,
+      default: () => { }
+    }
+  }
+
+}
+</script>
+
+<style lang="less" scoped>
+.navs {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 0.8rem;
+  color: #808080;
+}
+
+.item {
+  height: 5.2rem;
+  width: 20%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  .circle {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    width: 2.5rem;
+    height: 2.5rem;
+    margin-bottom: 0.3rem;
+  }
+  .iconfont {
+    font-size: 2rem;
+    color: #fff;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,20 +8,90 @@
 
     <!-- header导航 -->
     <Header></Header>
+
+    <!-- 各类导航 -->
+    <Nav :icon="icons"></Nav>
+
+    <!-- 猜你喜欢 -->
   </div>
 </template>
 
 <script>
 
 import Header from '@/components/Home/header'
+import Nav from '@/components/Home/navs'
 export default {
   name: 'Home',
   components: {
-    Header
+    Header,
+    Nav
   },
   data () {
     return {
-      loading: true
+      loading: true,
+      // nav 图标
+      icons: [
+        {
+          icon: 'icon-meishi',
+          bg: '#FD9D21',
+          url: '#',
+          text: '美食'
+        },
+        {
+          icon: 'icon-KTV',
+          bg: '#8A90FA',
+          url: '#',
+          text: '美食'
+        },
+        {
+          icon: 'icon-zhoubianyou',
+          bg: '#FED030',
+          url: '#',
+          text: '美食'
+        },
+        {
+          icon: 'icon-waimai',
+          bg: '#FD9D21',
+          url: '#',
+          text: '美食'
+        },
+        {
+          icon: 'icon-jiudian',
+          bg: '#FD9D21',
+          url: '#',
+          text: '美食'
+        },
+        {
+          icon: 'icon-xiuxian',
+          bg: '#FED030',
+          url: '#',
+          text: '美食'
+        },
+        {
+          icon: 'icon-dianzimaoyan-',
+          bg: '#FF6767',
+          url: '#',
+          text: '猫眼'
+        },
+        {
+          icon: 'icon-liren',
+          bg: '#FF80C2',
+          url: '#',
+          text: '美食'
+        },
+        {
+          icon: 'icon-meishi',
+          bg: '#FD9D21',
+          url: '#',
+          text: '小吃快餐'
+        },
+        {
+          icon: 'icon-qita',
+          bg: '#00D3BE',
+          url: '#',
+          text: '全部分类'
+        }
+      ]
     }
   },
   mounted () {
